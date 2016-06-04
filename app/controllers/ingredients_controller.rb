c2518f8b084b4e8de52226091e887e702e88c6d0
mghadley <matthew.g.hadley@gmail.com>
2016-06-04 10:50:05 -0600

    ingredients create action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,6 +3,21 @@ class IngredientsController < ApplicationController
   end
 
   def create
+    @recipe = current_user.recipes.find(params[:recipe_id])
+    20.times do |i|
+      @ingredient = Ingredient.find_or_initialize_by( params["name#{i}"])
+      if @ingredient.save
+        unless @measurement.create(recipe_id: @recipe.id, ingredient_id: @ingredient.id, amount: params["amount#{i}"], unit: params["unit#{i}"])
+          flash[:danger] = "Something went wrong"
+          render :new and return
+        end
+      else
+        flash[:danger] = "Something went wrong"
+        render :new and return
+      end
+    end
+    flash[:success] = "Ingredients added successfully"
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
